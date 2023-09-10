9792540f6a172d5b3e35758465811443be7e41da
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-09-10 21:13:26 +0530

    terraform_vpc&docdb_rds_elastic_infra

---
+++ main.tf
@@ -0,0 +1,54 @@
+resource "aws_elasticache_subnet_group" "elasticache_subnet_group" {
+  name       = "${var.env}-elasticache_subnet_group"
+  subnet_ids = var.subnet_ids
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-elasticache_subnet_group" }
+  )
+}
+
+resource "aws_security_group" "elasticache_sg" {
+  name        = "${var.env}-elasticache_security_group"
+  description = "${var.env}-elasticache_security_group"
+  vpc_id      = var.vpc_id
+
+
+  ingress {
+    description      = "elasticache"
+    from_port        = 6379
+    to_port          = 6379
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_cidr
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-elasticache_security_group" }
+  )
+
+}
+
+resource "aws_elasticache_replication_group" "elasticache_replication_group" {
+  replication_group_id        = "${var.env}-elasticache_rg"
+  description                 = "${var.env}-elasticache_rg"
+  node_type                   = var.node_type
+  num_node_groups             = var.num_node_groups
+  replicas_per_node_group     = var.replicas_per_node_group
+  port                        = 6379
+  subnet_group_name           = aws_elasticache_subnet_group.elasticache_subnet_group.name
+  security_group_ids = [aws_security_group.elasticache_sg.id]
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-elasticache_replication_group" }
+  )
+}
---
+++ vars.tf
@@ -0,0 +1,7 @@
+variable "subnet_ids" {}
+variable "env" {}
+variable "vpc_id" {}
+variable "allow_cidr" {}
+variable "node_type" {}
+variable "num_node_groups" {}
+variable "replicas_per_node_group" {}
